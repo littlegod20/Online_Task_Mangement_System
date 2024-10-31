0c29a8b49feea504a60468f27cacafd0393da378
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2024-10-31 13:10:19 +0000

    created a permissions utility to ensure the appropriate permissions for the appropriate role

---
+++ dist/utils/permissions.js
@@ -0,0 +1,10 @@
+"use strict";
+const PERMISSIONS = {
+    user: [
+        "create:own_tasks",
+        "update:own_task",
+        "delete:own_tasks",
+        "view:own_tasks",
+    ],
+    admin: ["update:all_task", "delete:all_tasks", "view:all_tasks"],
+};
---
+++ src/utils/permissions.ts
@@ -0,0 +1,9 @@
+const PERMISSIONS = {
+  user: [
+    "create:own_tasks",
+    "update:own_task",
+    "delete:own_tasks",
+    "view:own_tasks",
+  ],
+  admin: ["update:all_task", "delete:all_tasks", "view:all_tasks"],
+};
