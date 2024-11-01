3c37dab6a69052996a6c38520c7e6a5c1a3edb71
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2024-11-01 00:24:11 +0000

    changed 'Permission denied' to 'unauthorized'

--- dist/middlewares/rolePermissions.js
+++ dist/middlewares/rolePermissions.js
@@ -14,7 +14,7 @@ const hasPermission = (permission) => {
             next();
         }
         else {
-            res.status(403).json({ message: "Permission denied" });
+            res.status(403).json({ message: "unauthorized" });
         }
     };
 };
